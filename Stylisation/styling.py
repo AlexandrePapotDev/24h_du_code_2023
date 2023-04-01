f18b74bff894e34c2d01f24e23d6feada0da0d25
grayspot <grayspot@inbox.ru>
2023-04-01 19:28:58 +0200

    Update part 3

--- Stylisation/styling.py
+++ Stylisation/styling.py
@@ -2,14 +2,42 @@ import glob
 import numpy as np
 from PIL import Image
 import tensorflow as tf
-# import tensorflow_hub as hub
+import torch
 
-model = tf.saved_model.load('../models/fnst')
+
+def image_from_prompt(path, prompt):
+
+    pipe = torch.load("./models/text2style.pt")
+
+    image = Image.open(path).convert("RGB")
+
+    max_size = 750
+    width, height = image.size
+    if width > max_size or height > max_size:
+        if width > height:
+            new_width = max_size
+            new_height = int(height * (max_size / width))
+        else:
+            new_height = max_size
+            new_width = int(width * (max_size / height))
+        new_size = (new_width, new_height)
+        image = image.resize(new_size)
+
+    images = pipe(prompt, image=image, num_inference_steps=10, image_guidance_scale=1.5, guidance_scale=2.5).images
+
+    nb_files = len(glob.glob("./output/*"))
+    output_path = 'output/result'+str(nb_files+1)+'.jpg'
+    images[0].save(output_path)
+
+
+# image_from_prompt('./examples/input.jpg', 'make the car red')
 # tf.saved_model.save(style_transfer_model, './models/fnst')
 
 
-def perform_style_transfer(content_path, style_path, output_path):
-    
+def perform_style_transfer(content_path, style_path):
+
+    model = tf.saved_model.load('../models/fnst')
+
     content_image = np.array(Image.open(content_path).convert('RGB'))
     style_image = np.array(Image.open(style_path).convert('RGB'))
 
@@ -18,13 +46,13 @@ def perform_style_transfer(content_path, style_path, output_path):
     
     output = model(content_image, style_image)
     stylized_image = output[0]
-    
+
+    nb_files = len(glob.glob("./output/*"))
+    output_path = 'output/result'+str(nb_files+1)+'.jpg'
+
     stylized_image = np.uint8(stylized_image[0] * 255)
     Image.fromarray(stylized_image).save(output_path)
 
-
-nb_files = len(glob.glob("./output/*"))
-output_path = 'output/result'+str(nb_files+1)+'.jpg'
 
 # content_path = 'input/input.jpg'
 # style_path = 'input/style_anime.jpg'
