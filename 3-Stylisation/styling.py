4ff5dddcbebe9bb7f73b76ffdf4822d621497306
grayspot <grayspot@inbox.ru>
2023-04-01 12:54:51 +0200

    Update part 3

---
+++ 3-Stylisation/styling.py
@@ -0,0 +1,28 @@
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+import tensorflow_hub as hub
+
+model = tf.saved_model.load('../models/fnst')
+#tf.saved_model.save(style_transfer_model, './models/fnst')
+
+def perform_style_transfer(content_path, style_path, output_path):
+    
+    content_image = np.array(Image.open(content_path).convert('RGB'))
+    style_image = np.array(Image.open(style_path).convert('RGB'))
+
+    content_image = tf.convert_to_tensor(content_image, np.float32)[tf.newaxis, ...] / 255.
+    style_image = tf.convert_to_tensor(style_image, np.float32)[tf.newaxis, ...] / 255.
+    
+    output = model(content_image, style_image)
+    stylized_image = output[0]
+    
+    stylized_image = np.uint8(stylized_image[0] * 255)
+    Image.fromarray(stylized_image).save(output_path)
+
+
+#content_path = 'examples/input.jpg'
+#style_path = 'examples/style.jpg'
+#output_path = 'examples/result.jpg'
+
+#perform_style_transfer(content_path, style_path, output_path)
