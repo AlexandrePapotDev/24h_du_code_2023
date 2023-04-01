6159b00bfde1bfeca38c099262eb16f67c093239
Dursam <48018775+Dursam@users.noreply.github.com>
2023-04-01 19:09:04 +0200

    Add Correction process

--- 2-Correction/selection.py
+++
@@ -1,4 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-
-tokenizer = AutoTokenizer.from_pretrained("CIDAS/clipseg-rd64-refined")
-model = AutoModelForSeq2SeqLM.from_pretrained("CIDAS/clipseg-rd64-refined")
