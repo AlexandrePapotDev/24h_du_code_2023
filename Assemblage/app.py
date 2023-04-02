b55470b39245ce90ee2fcae61c8127f83aac8268
Valentin GIROD <valentin.girod.etu@univ-lemans.fr>
2023-04-02 08:35:49 +0200

    ajout du nouveau modèle de stylisation

--- Assemblage/app.py
+++ Assemblage/app.py
@@ -8,9 +8,10 @@ from Classification.imageClassifier import predictClass
 from Correction.correction import correction
 import glob
 from collections import defaultdict
+import subprocess
 
-content_path = '../Stylisation/input/'
-style_path = '../Stylisation/input/'
+content_path = 'input/'
+style_path = 'input/'
 output_path = 'output/'
 
 
@@ -105,14 +106,59 @@ def Correction():
 
 
 # Define the function to display the content for page 3
-def Stylisation():
+def Stylisation2():
     show_button = False
-    st.title("Stylisation")
+    st.title("Stylisation lente mais qualitative")
     image1 = False
     image2 = False
     name_image1 = None
     name_image2 = None
-    object_img1 = None
+    def show_button():
+        if(st.button("Générer une image à partir d'un filtre")):
+            #perform_style_transfer(content_path+name_image1, style_path+name_image2)
+            style_weight = "2e2"
+            content_weight = "1e4"   
+            command = f"python3 ../models/pytorchNeuralStyleTransfer/neural_style_transfer.py --content_img_name {name_image1} --style_img_name {name_image2} --style_weight {style_weight} --content_weight {content_weight}"
+            subprocess.call(command, shell=True)
+            #load image at output_path+"output_"+name_image1
+            name_image = name_image1.split(".")[0]+"_"+name_image2.split(".")[0]+'.'+name_image1.split(".")[1]
+            image = Image.open(output_path+name_image)
+            st.image(image, caption="Image stylisée")
+    # make a text input
+
+
+    col1, col2 = st.columns(2)
+    with col1:        
+        param1 = st.text_input("Alpha", "")   
+        uploaded_file = st.file_uploader("Choisissez une image...", type=["jpg", "jpeg", "png"])
+        if uploaded_file is not None:
+            image = Image.open(uploaded_file)
+            st.image(image, caption="Uploaded Image")
+            image.save(content_path+uploaded_file.name)
+            name_image1 = uploaded_file.name
+            image1 = True
+
+    with col2:
+        param2 = st.text_input("Beta", "")   
+        uploaded_file2 = st.file_uploader("Choisissez un filtre...", type=["jpg", "jpeg", "png"])
+        if uploaded_file2 is not None:
+            image = Image.open(uploaded_file2)
+            st.image(image, caption="Uploaded Image")
+            image.save(content_path+uploaded_file2.name)
+            name_image2 = uploaded_file2.name
+            image2 = True
+    if(image2 and image1):
+        show_button()
+
+
+# Define the function to display the content for page 3
+def Stylisation():
+    show_button = False
+    st.title("Stylisation rapide mais peu qualitative")
+    image1 = False
+    image2 = False
+    name_image1 = None
+    name_image2 = None
     def show_button():
         if(st.button("Générer une image à partir d'un filtre")):
             perform_style_transfer(content_path+name_image1, style_path+name_image2)
@@ -125,7 +171,7 @@ def Stylisation():
     textinput = st.text_input("Entrez un prompt", "")
 
     col1, col2 = st.columns(2)
-    with col1:        
+    with col1:
         uploaded_file = st.file_uploader("Choisissez une image...", type=["jpg", "jpeg", "png"])
         if uploaded_file is not None:
             image = Image.open(uploaded_file)
@@ -133,7 +179,6 @@ def Stylisation():
             image.save(content_path+uploaded_file.name)
             name_image1 = uploaded_file.name
             image1 = True
-            object_img1 = image
 
     with col2:
         uploaded_file2 = st.file_uploader("Choisissez un filtre...", type=["jpg", "jpeg", "png"])
@@ -183,6 +228,7 @@ st.markdown(
 classification_button = st.sidebar.button("Classification")
 correction_button = st.sidebar.button("Correction")
 stylisation_button = st.sidebar.button("Stylisation")
+stylisation2_button = st.sidebar.button("Stylisation2")
 
 # Define a variable to store the current page
 current_page = getPage()
@@ -197,6 +243,9 @@ if correction_button:
 if stylisation_button:
     current_page = "Stylisation"
     setPage("Stylisation")
+if stylisation2_button:
+    current_page = "Stylisation2"
+    setPage("Stylisation2")
 # show current page
 locals()[current_page]()
 
